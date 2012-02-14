a0d9b3890bd701ba783e98378672e1e2c04a8d8c
Leo Chen <leo.hj.chen@gmail.com>
2012-02-15 01:16:14 +0800

    user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   #everything accessible through the web has to be in attr_accessible
   attr_accessible :name, :email, :password, :password_confirmation
-  attr_accessor :password
+  attr_accessor :password # accessor created automatically by rails for fields in DB
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -14,7 +14,37 @@ class User < ActiveRecord::Base
                        :confirmation => true,
                        :length => { :within => 6..40 }
   
+  #callback to encrypt password before user is saved (make callback method private)
+  before_save :encrypt_password
   
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil? #if no user with email address
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+  
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
   
 end
 # == Schema Information
